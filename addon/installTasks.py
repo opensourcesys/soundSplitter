b3ddac98d70a8487c1ccd1b1467d6c02838e4458
Joseph Lee <joseph.lee22590@gmail.com>
2022-12-28 15:02:59 -0800

    Add install tasks to present a message if attempting to install on unsupported Windows releaes

---
+++ addon/installTasks.py
@@ -0,0 +1,40 @@
+# SoundSplitter/installTasks.py
+# Copyright 2022 Joseph Lee, released under GPL.
+
+# Provides needed routines during add-on installation and removal.
+# Mostly checks compatibility.
+# Routines are partly based on other add-ons,
+# particularly Place Markers by Noelia Martinez (thanks add-on authors).
+
+import addonHandler
+addonHandler.initTranslation()
+
+
+def onInstall():
+	import gui
+	import wx
+	import winVersion
+	import globalVars
+	# Do not present dialogs if minimal mode is set.
+	currentWinVer = winVersion.getWinVer()
+	# Sound Splitter requires Windows 10 or later.
+	# Translators: title of the error dialog shown when trying to install the add-on in unsupported systems.
+	# Unsupported systems include Windows versions earlier than 10 and unsupported feature updates.
+	unsupportedWindowsReleaseTitle = _("Unsupported Windows release")
+	minimumWinVer = winVersion.WIN10
+	if currentWinVer < minimumWinVer:
+		if not globalVars.appArgs.minimal:
+			gui.messageBox(
+				_(
+					# Translators: Dialog text shown when trying to install the add-on on
+					# releases earlier than minimum supported release.
+					"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
+					"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
+				).format(
+					releaseName=currentWinVer.releaseName,
+					build=currentWinVer.build,
+					supportedReleaseName="Windows10",
+					supportedBuild=minimumWinVer.build,
+				), unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR
+			)
+		raise RuntimeError("Attempting to install Sound Splitter on Windows releases earlier than 10")
